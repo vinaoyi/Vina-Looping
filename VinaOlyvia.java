e3939d01e076308b0b939f506088ca82c1be16a5
vinaoyi <39687742+vinaoyi@users.noreply.github.com>
2018-05-28 13:33:51 +0800

    Add files via upload

---
+++ VinaOlyvia.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vinaolyvia;
+
+/**
+ *
+ * @author Atri
+ */
+public class VinaOlyvia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for(int i=1;i<=10;i++)
+            System.out.println( i + " * 8 = " + (i*8));
+    }
+}
